704fe9447bf9988324c0f982d54ab61d468bb5fe
Georgy Savva <georgy.savva@nyu.edu>
2026-02-17 07:20:27 -0800

    clean up

--- run_gt_vlm_eval.sh
+++
@@ -1,7 +0,0 @@
-python run_eval.py mc_multiplayer_v2_eval_new_sneak_combined/translationEval --num-trials 3
-python run_eval.py mc_multiplayer_v2_eval_new_sneak_combined/rotationEval --num-trials 3
-python run_eval.py mc_multiplayer_v2_eval_new_sneak_combined/structureEval --num-trials 3
-python run_eval.py mc_multiplayer_v2_eval_new_sneak_combined/turnToLookEval --num-trials 3
-python run_eval.py mc_multiplayer_v2_eval_new_sneak_combined/turnToLookOppositeEval --num-trials 3
-python run_eval.py mc_multiplayer_v2_eval_new_sneak_combined/oneLooksAwayEval --num-trials 3
-python run_eval.py mc_multiplayer_v2_eval_new_sneak_combined/bothLookAwayEval --num-trials 3
